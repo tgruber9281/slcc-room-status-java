fcc53c0da8dff738a241ec53c87ee143120df37a
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-05-01 22:59:01 -0500

    Initial commit

---
+++ src/main/java/com/stlouiscatclinic/roomstatus/RoomStatusApplication.java
@@ -0,0 +1,13 @@
+package com.stlouiscatclinic.roomstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stlouiscatclinic/roomstatus/RoomStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stlouiscatclinic.roomstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
